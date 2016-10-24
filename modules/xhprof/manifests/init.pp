775af0c6f96f6b61545f2b4f5f044c86cd701aaf
Bronson Quick <bronson@sennza.com.au>
2016-10-24 11:09:43 +1000

    Add in some extra checks

--- modules/xhprof/manifests/init.pp
+++ modules/xhprof/manifests/init.pp
@@ -53,7 +53,7 @@ class xhprof (
     path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
     command => 'git clone https://github.com/perftools/xhgui.git /vagrant/extensions/xhprof/xhgui',
     require => Package[ 'git-core' ],
-    unless  => 'ls /vagrant/extensions/xhprof/xhgui'
+    unless  => 'test -d /vagrant/extensions/xhprof/xhgui'
   }
   exec { 'install composer':
     path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
@@ -66,13 +66,15 @@ class xhprof (
     environment => [ 'COMPOSER_HOME=/usr/bin/composer' ],
     path        => [ '/usr/bin/' ],
     command     => 'composer require alcaeus/mongo-php-adapter --ignore-platform-reqs',
-    require     => Exec[ 'install composer' ]
+    require     => Exec[ 'install composer' ],
+    unless      => 'composer show alcaeus/mongo-php-adapter'
   }
   exec { 'install xhgui':
     path    => [ '/usr/bin/' ],
     cwd     => '/vagrant/extensions/xhprof/xhgui/',
     command => [ 'php install.php' ],
-    require => [ Exec['clone xhgui'], Exec['install composer'] ]
+    require => [ Exec['clone xhgui'], Exec['install composer'] ],
+    unless  => 'test -d /vagrant/extensions/xhprof/xhgui'
   }
   file { '/vagrant/xhgui':
     ensure => link,
