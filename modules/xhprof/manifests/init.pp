bb60614e0e835432c66230322cca073c8f54b486
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-10 09:56:43 +1000

    Fix alignment

--- modules/xhprof/manifests/init.pp
+++ modules/xhprof/manifests/init.pp
@@ -12,17 +12,17 @@ class xhprof (
 	}
 
   exec { 'download xhprof and build it':
-    path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-    command     =>
+    path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+    command =>
       'curl -L https://github.com/humanmade/xhprof/archive/sampling-interval.zip > /tmp/xhprof.zip && unzip -o /tmp/xhprof.zip -d /tmp && cd /tmp/xhprof-sampling-interval/extension && phpize && ./configure && make && make install',
-    require     => [ Package['curl'], Package["php${config[php]}-dev"] ],
-    unless      => 'php -m | grep xhprof',
+    require => [ Package['curl'], Package["php${config[php]}-dev"] ],
+    unless  => 'php -m | grep xhprof',
   }
 
 	exec { 'move module':
-		path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-		command     => 'cp /tmp/xhprof-sampling-interval/extension/modules/xhprof.so /usr/lib/php/20151012',
-		require     => Exec['download xhprof and build it']
+		path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+		command => 'cp /tmp/xhprof-sampling-interval/extension/modules/xhprof.so /usr/lib/php/20151012',
+		require => Exec['download xhprof and build it']
 	}
 
 	file { [
