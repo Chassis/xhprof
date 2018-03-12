aafcf0bdd8e0cb1868dea65377ba2591b95ef952
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-12 10:45:53 +1000

    Add disabled_extension support

--- modules/xhprof/manifests/init.pp
+++ modules/xhprof/manifests/init.pp
@@ -4,38 +4,61 @@ class xhprof (
   $path = '/vagrant/extensions/xhprof'
 ) {
 
-	if ! defined( Package["${config[php]}-dev"] ) {
-		package { "php${config[php]}-dev":
-			ensure  => latest,
-			require => Package["php${config[php]}-fpm"]
-		}
+	if ( ! empty( $config[disabled_extensions] ) and 'chassis/xhprof' in $config[disabled_extensions] ) {
+		$package = absent
+		$file = absent
+	} else {
+		$package = latest
+		$file = file
 	}
 
-  exec { 'download xhprof and build it':
-    path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-    command =>
-      'curl -L https://github.com/humanmade/xhprof/archive/sampling-interval.zip > /tmp/xhprof.zip && unzip -o /tmp/xhprof.zip -d /tmp && cd /tmp/xhprof-sampling-interval/extension && phpize && ./configure && make && make install',
-    require => [ Package['curl'], Package["php${config[php]}-dev"] ],
-    unless  => 'php -m | grep xhprof',
-  }
-
-	exec { 'move module':
-		path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-		command => 'cp /tmp/xhprof-sampling-interval/extension/modules/xhprof.so /usr/lib/php/20151012',
-		require => Exec['download xhprof and build it'],
-		unless  => 'test -f /usr/lib/php/20151012/xhprof.so'
+	if ! defined( Package["${config[php]}-dev"] ) {
+		package { "php${config[php]}-dev":
+			ensure  => $package,
+			require => Package["php${config[php]}-fpm"]
+		}
 	}
 
 	file { [
 		"/etc/php/${config[php]}/fpm/conf.d/xhprof.ini",
 		"/etc/php/${config[php]}/cli/conf.d/xhprof.ini",
 	]:
-		ensure  => file,
+		ensure  => $file,
 		content => template('xhprof/xhprof.ini.erb'),
 		owner   => 'root',
 		group   => 'root',
 		mode    => '0644',
 		require => [ Package["php${config[php]}-fpm"] ],
 		notify  => Service["php${config[php]}-fpm"]
+	}
+
+	if ( latest == $package ) {
+		exec { 'download xhprof and build it':
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command =>
+				'curl -L https://github.com/humanmade/xhprof/archive/sampling-interval.zip > /tmp/xhprof.zip && unzip -o /tmp/xhprof.zip -d /tmp && cd /tmp/xhprof-sampling-interval/extension && phpize && ./configure && make && make install'
+			,
+			require => [ Package['curl'], Package["php${config[php]}-dev"] ],
+			unless  => 'php -m | grep xhprof',
+		}
+
+		exec { 'move module':
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command =>
+				'cp /tmp/xhprof-sampling-interval/extension/modules/xhprof.so /usr/lib/php/20151012'
+			,
+			require => Exec['download xhprof and build it'],
+			unless  => 'test -f /usr/lib/php/20151012/xhprof.so'
+		}
+	} else {
+		file { [
+			'/usr/lib/php/20151012/xhprof.so',
+			'/tmp/xhprof.zip',
+			'/tmp/xhprof-sampling-interval'
+			]:
+			ensure  =>  $file,
+			recurse => true,
+			force   => true
+		}
 	}
 }
