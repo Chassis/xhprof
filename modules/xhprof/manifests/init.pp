1135ad55d1a08539d121e0f708b52825e32ea68e
Bronson Quick <bronson@bronsonquick.com.au>
2018-08-02 14:56:44 +1000

    Add a before parameter to make sure php-fpm is restarted after we've setup xhprof

--- modules/xhprof/manifests/init.pp
+++ modules/xhprof/manifests/init.pp
@@ -20,26 +20,22 @@ class xhprof (
 		}
 	}
 
-	file { [
-		"/etc/php/${config[php]}/fpm/conf.d/xhprof.ini",
-		"/etc/php/${config[php]}/cli/conf.d/xhprof.ini",
-	]:
-		ensure  => $file,
-		content => template('xhprof/xhprof.ini.erb'),
-		owner   => 'root',
-		group   => 'root',
-		mode    => '0644',
-		require => [ Package["php${config[php]}-fpm"] ],
-		notify  => Service["php${config[php]}-fpm"]
-	}
-
 	if ( latest == $package ) {
 		exec { 'download xhprof and build it':
 			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
 			command =>
-				'curl -L https://github.com/humanmade/xhprof/archive/sampling-interval.zip > /tmp/xhprof.zip && unzip -o /tmp/xhprof.zip -d /tmp && cd /tmp/xhprof-sampling-interval/extension && phpize && ./configure && make && make install; cp /tmp/xhprof-sampling-interval/extension/modules/xhprof.so /usr/lib/php/20151012',
-			require => [ Package['curl'], Package["php${config[php]}-dev"] ],
+				'curl -L https://github.com/humanmade/xhprof/archive/sampling-interval.zip > /tmp/xhprof.zip && unzip -o /tmp/xhprof.zip -d /tmp && cd /tmp/xhprof-sampling-interval/extension && phpize && ./configure && make && make install',
+			require => Package["php${config[php]}-dev"],
 			unless  => 'php -m | grep xhprof',
+			before  => [ File["/etc/php/${config[php]}/fpm/conf.d/xhprof.ini"], File["/etc/php/${config[php]}/cli/conf.d/xhprof.ini"] ]
+		}
+
+		exec { 'move module':
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command =>
+			'cp /tmp/xhprof-sampling-interval/extension/modules/xhprof.so /usr/lib/php/20151012',
+			require => Exec['download xhprof and build it'],
+			unless  => 'test -f /usr/lib/php/20151012/xhprof.so',
 		}
 	} else {
 		file { [
@@ -51,6 +47,18 @@ class xhprof (
 		  recurse => true,
 		  force   => true
 		}
+	}
+
+	file { [
+		"/etc/php/${config[php]}/fpm/conf.d/xhprof.ini",
+		"/etc/php/${config[php]}/cli/conf.d/xhprof.ini",
+	]:
+		ensure  => $file,
+		content => template('xhprof/xhprof.ini.erb'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0644',
+		notify  => Service["php${config[php]}-fpm"]
 	}
 
 }
